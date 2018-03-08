6474fada37274fabd36ab644a72cb60314a536f0
Bennett Piater <bennett@piater.name>
2018-03-08 13:09:01 +0100

    unary-ops: add unit tests for tdd

---
+++ test/tdd_parser_unops.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#include "mCc/ast.h"
+#include "mCc/parser.h"
+
+TEST(TDD_PARSER_UNOPS, NEG)
+{
+	const char str[] = "-5";
+	auto result = mCc_parser_parse_string(str);
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_UNARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_UNARY_OP_NEG, expr->unary_op);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(TDD_PARSER_UNOPS, NOT)
+{
+	const char str[] = "!true";
+	auto result = mCc_parser_parse_string(str);
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_UNARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_UNARY_OP_NOT, expr->unary_op);
+
+	mCc_ast_delete_expression(expr);
+}
