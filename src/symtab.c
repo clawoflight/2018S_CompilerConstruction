10a6dde2fe81e5a373f4497107af75720a833f4b
Bennett Piater <bennett@piater.name>
2018-04-18 10:02:48 +0200

    format

--- src/symtab.c
+++ src/symtab.c
@@ -346,43 +346,42 @@ enum MCC_SYMTAB_SCOPE_LINK_ERROR
 mCc_symtab_scope_link_ref_assignment(struct mCc_symtab_scope *self,
                                      struct mCc_ast_statement *stmt)
 {
-    // Get the ID to link
-    struct mCc_ast_identifier *id;
-    switch (stmt->type) {
-        case MCC_AST_STATEMENT_TYPE_ASSGN: id = stmt->id_assgn; break;
-        default: return MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT;
-    }
+	// Get the ID to link
+	struct mCc_ast_identifier *id;
+	switch (stmt->type) {
+	case MCC_AST_STATEMENT_TYPE_ASSGN: id = stmt->id_assgn; break;
+	default: return MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT;
+	}
 
-    struct mCc_symtab_entry *entry = mCc_symtab_scope_lookup_id(self, id);
-    if (!entry)
-        return MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID;
+	struct mCc_symtab_entry *entry = mCc_symtab_scope_lookup_id(self, id);
+	if (!entry)
+		return MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID;
 
-    //MCC_SYMTAB_SCOPE_LINK_ERR_VAR
-    //MCC_SYMTAB_SCOPE_LINK_ERR_FUN_WITHOUT_CALL
+	// MCC_SYMTAB_SCOPE_LINK_ERR_VAR
+	// MCC_SYMTAB_SCOPE_LINK_ERR_FUN_WITHOUT_CALL
 
-    // Basic error checking, though not full type checking
-    switch (entry->entry_type) {
-        case MCC_SYMTAB_ENTRY_TYPE_FUNC:
-			// if id_assgn is registered as a function, it's illegal to assign to it
-            return MCC_SYMTAB_SCOPE_LINK_ERR_ASSIGN_TO_FUNCTION;
+	// Basic error checking, though not full type checking
+	switch (entry->entry_type) {
+	case MCC_SYMTAB_ENTRY_TYPE_FUNC:
+		// if id_assgn is registered as a function, it's illegal to assign to it
+		return MCC_SYMTAB_SCOPE_LINK_ERR_ASSIGN_TO_FUNCTION;
 
-        case MCC_SYMTAB_ENTRY_TYPE_ARR:
-			// Illegal to assign to an array without also giving an index
-            if(!stmt->lhs_assgn)
-                return MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS;
-			break;
+	case MCC_SYMTAB_ENTRY_TYPE_ARR:
+		// Illegal to assign to an array without also giving an index
+		if (!stmt->lhs_assgn)
+			return MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS;
+		break;
 
-        case MCC_SYMTAB_ENTRY_TYPE_VAR:
-            if (stmt->expression->type != MCC_AST_EXPRESSION_TYPE_IDENTIFIER)
-                return MCC_SYMTAB_SCOPE_LINK_ERR_VAR;
-            // here second if for UNDECLARED_ID ?
-            break;
-    }
-    // TODO: Link in identifier
-   // id->symtab_ref = entry;
-   // return MCC_SYMTAB_SCOPE_LINK_ERR_OK;
+	case MCC_SYMTAB_ENTRY_TYPE_VAR:
+		if (stmt->expression->type != MCC_AST_EXPRESSION_TYPE_IDENTIFIER)
+			return MCC_SYMTAB_SCOPE_LINK_ERR_VAR;
+		// here second if for UNDECLARED_ID ?
+		break;
+	}
+	// TODO: Link in identifier
+	// id->symtab_ref = entry;
+	// return MCC_SYMTAB_SCOPE_LINK_ERR_OK;
 }
-
 
 /******************************* Destructors */
 static void mCc_symtab_delete_scope(struct mCc_symtab_scope *scope)
