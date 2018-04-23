e1176974052a66b39b83bac4004ff80744194f8d
Bennett Piater <bennett@piater.name>
2018-04-23 10:02:00 +0200

    clang-format

--- src/ast_symtab_link.c
+++ src/ast_symtab_link.c
@@ -33,42 +33,42 @@ static void handle_assign(struct mCc_ast_statement *stmt, void *data)
 	case MCC_SYMTAB_SCOPE_LINK_ERR_OK: return;
 	case MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID:
 		if (snprintf(tmp_result.err_msg, err_len, "Use of undeclared id: '%s'",
-		             stmt->id_assgn->id_value)){
+		             stmt->id_assgn->id_value)) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = stmt->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
+		}
 		break;
 	case MCC_SYMTAB_SCOPE_LINK_ERR_ASSIGN_TO_FUNCTION:
 		if (snprintf(tmp_result.err_msg, err_len,
 		             "Assignment to function name: '%s'",
-		             stmt->id_assgn->id_value)){
+		             stmt->id_assgn->id_value)) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = stmt->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
+		}
 		break;
 	case MCC_SYMTAB_SCOPE_LINK_ERR_VAR:
 		if (snprintf(tmp_result.err_msg, err_len,
 		             "Use of subscript on variable: '%s'",
-		             stmt->id_assgn->id_value)){
+		             stmt->id_assgn->id_value)) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = stmt->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
+		}
 		break;
 	case MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS:
 		if (snprintf(tmp_result.err_msg, err_len,
 		             "Use of array without subscript: '%s'",
-		             stmt->id_assgn->id_value)){
+		             stmt->id_assgn->id_value)) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = stmt->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
+		}
 		break;
 	case MCC_SYMTAB_SCOPE_LINK_ERR_FUN_WITHOUT_CALL:
 	case MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT:
@@ -88,63 +88,63 @@ static void handle_expression(struct mCc_ast_expression *expr, void *data)
 {
 	if (tmp_result.status)
 		return; // Return if an error happened
-	struct mCc_symtab_scope *scope = *(struct mCc_symtab_scope **) data;
+	struct mCc_symtab_scope *scope = *(struct mCc_symtab_scope **)data;
 
 	enum MCC_SYMTAB_SCOPE_LINK_ERROR retval =
 	    mCc_symtab_scope_link_ref_expression(scope, expr);
 
 	switch (retval) {
-        case MCC_SYMTAB_SCOPE_LINK_ERR_OK: return;
-        case MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID:
-            if (snprintf(tmp_result.err_msg, err_len, "Use of undeclared id: '%s'",
-                         expr->identifier->id_value)){
-                if (tmp_result.err_msg[0]) {
-                    tmp_result.err_loc = expr->node.sloc;
-                    tmp_result.status = 1;
-                }
-            }
-            break;
-        case MCC_SYMTAB_SCOPE_LINK_ERR_ASSIGN_TO_FUNCTION:
-            if (snprintf(tmp_result.err_msg, err_len,
-                         "Assignment to function name: '%s'",
-                         expr->identifier->id_value)) {
-                if (tmp_result.err_msg[0]) {
-                    tmp_result.err_loc = expr->node.sloc;
-                    tmp_result.status = 1;
-                }
-            }
-            break;
-		case MCC_SYMTAB_SCOPE_LINK_ERR_FUN_WITHOUT_CALL:
-			if (snprintf(tmp_result.err_msg, err_len, "Use of a function without a call: '%d'",
-						 expr->type)) {
-				if (tmp_result.err_msg[0]) {
-					tmp_result.err_loc = expr->node.sloc;
-					tmp_result.status = 1;
-				}
-            }
-			break;
-		case MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS:
-			if (snprintf(tmp_result.err_msg, err_len, "Use of an array without brackets: '%d'",
-						 expr->type)){
-				if (tmp_result.err_msg[0]) {
-					tmp_result.err_loc = expr->node.sloc;
-					tmp_result.status = 1;
-				}
-            }
-			break;
-		case MCC_SYMTAB_SCOPE_LINK_ERR_VAR:
-			if (snprintf(tmp_result.err_msg, err_len, "Use of undeclared variable: '%d'",
-						 expr->type)){
-				if (tmp_result.err_msg[0]) {
-					tmp_result.err_loc = expr->node.sloc;
-					tmp_result.status = 1;
-				}
-            }
-			break;
-		case MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT:
-			strcpy(tmp_result.err_msg,
-				   "Development error! This error should not haave happened here.");
-			break;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_OK: return;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID:
+		if (snprintf(tmp_result.err_msg, err_len, "Use of undeclared id: '%s'",
+		             expr->identifier->id_value)) {
+			if (tmp_result.err_msg[0]) {
+				tmp_result.err_loc = expr->node.sloc;
+				tmp_result.status = 1;
+			}
+		}
+		break;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_ASSIGN_TO_FUNCTION:
+		if (snprintf(tmp_result.err_msg, err_len,
+		             "Assignment to function name: '%s'",
+		             expr->identifier->id_value)) {
+			if (tmp_result.err_msg[0]) {
+				tmp_result.err_loc = expr->node.sloc;
+				tmp_result.status = 1;
+			}
+		}
+		break;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_FUN_WITHOUT_CALL:
+		if (snprintf(tmp_result.err_msg, err_len,
+		             "Use of a function without a call: '%d'", expr->type)) {
+			if (tmp_result.err_msg[0]) {
+				tmp_result.err_loc = expr->node.sloc;
+				tmp_result.status = 1;
+			}
+		}
+		break;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS:
+		if (snprintf(tmp_result.err_msg, err_len,
+		             "Use of an array without brackets: '%d'", expr->type)) {
+			if (tmp_result.err_msg[0]) {
+				tmp_result.err_loc = expr->node.sloc;
+				tmp_result.status = 1;
+			}
+		}
+		break;
+	case MCC_SYMTAB_SCOPE_LINK_ERR_VAR:
+		if (snprintf(tmp_result.err_msg, err_len,
+		             "Use of undeclared variable: '%d'", expr->type)) {
+			if (tmp_result.err_msg[0]) {
+				tmp_result.err_loc = expr->node.sloc;
+				tmp_result.status = 1;
+			}
+		}
+		break;
+	case MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT:
+		strcpy(tmp_result.err_msg,
+		       "Development error! This error should not haave happened here.");
+		break;
 	}
 
 	// If an error happened, set status
@@ -160,64 +160,65 @@ static void handle_declaration(struct mCc_ast_declaration *decl, void *data)
 {
 	if (tmp_result.status)
 		return; // Return if an error happened
-	struct mCc_symtab_scope *scope = (struct mCc_symtab_scope *) data;
+	struct mCc_symtab_scope *scope = (struct mCc_symtab_scope *)data;
 	int retval = mCc_symtab_scope_add_decl(scope, decl);
 	switch (retval) {
 	case 1:
 		if (snprintf(tmp_result.err_msg, err_len, "Redeclared id: '%s'",
-		             decl->decl_id->id_value) == -1){
+		             decl->decl_id->id_value) == -1) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = decl->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
-			;
+		};
 		break;
 	case -1: strcpy(tmp_result.err_msg, "Memory allocation error"); break;
 	default: break;
 	}
-	// If an error happened, set status //TODO check why this makes errors when uncommanting
+	// If an error happened, set status //TODO check why this makes errors when
+	// uncommanting
 	/*if (tmp_result.err_msg[0]) {
-		tmp_result.err_loc = decl->node.sloc;
-		tmp_result.status = 1;
+	    tmp_result.err_loc = decl->node.sloc;
+	    tmp_result.status = 1;
 	}*/
 }
 
 static void handle_func_def(struct mCc_ast_function_def *fn, void *data)
 {
 	if (tmp_result.status)
-		return;            // Return if an error happened
-	struct mCc_symtab_scope *scope = *(struct mCc_symtab_scope **) data;
+		return; // Return if an error happened
+	struct mCc_symtab_scope *scope = *(struct mCc_symtab_scope **)data;
 	int retval = mCc_symtab_scope_add_func_def(scope, fn);
 
 	switch (retval) {
 	case 1:
 		if (snprintf(tmp_result.err_msg, err_len, "Redeclared function '%s'",
-		             fn->identifier->id_value) == -1){
+		             fn->identifier->id_value) == -1) {
 			if (tmp_result.err_msg[0]) {
 				tmp_result.err_loc = fn->node.sloc;
 				tmp_result.status = 1;
 			}
-        }
-			;
+		};
 		break;
 	case -1: strcpy(tmp_result.err_msg, "Memory allocation error"); break;
 	default: break;
 	}
-    // If an error happened, set status     //TODO check why this makes errors when uncommanting
-    /*if (tmp_result.err_msg[0]) {
-        tmp_result.err_loc = fn->node.sloc;
-        tmp_result.status = 1;
-    }*/
+	// If an error happened, set status     //TODO check why this makes errors
+	// when uncommanting
+	/*if (tmp_result.err_msg[0]) {
+	    tmp_result.err_loc = fn->node.sloc;
+	    tmp_result.status = 1;
+	}*/
 }
 
 /***************************************************** Main things */
 
-static struct mCc_ast_visitor symtab_visitor(struct mCc_symtab_scope *curr_scope_ptr)
+static struct mCc_ast_visitor
+symtab_visitor(struct mCc_symtab_scope *curr_scope_ptr)
 {
 	return (struct mCc_ast_visitor){ .traversal = MCC_AST_VISIT_DEPTH_FIRST,
 		                             .order = MCC_AST_VISIT_PRE_ORDER,
-									 .mode = MCC_AST_VISIT_MODE_SYMTAB_REF,
+		                             .mode = MCC_AST_VISIT_MODE_SYMTAB_REF,
 
 		                             // Important!
 		                             .userdata = curr_scope_ptr,
@@ -260,8 +261,9 @@ mCc_ast_symtab_build(struct mCc_ast_program *program)
 		// TODO set error msg and status and return
 	}
 
-	// This will be used by the callbacks to store the scope they are currently in
-	struct mCc_symtab_scope *curr_scope_ptr= root_scope;
+	// This will be used by the callbacks to store the scope they are currently
+	// in
+	struct mCc_symtab_scope *curr_scope_ptr = root_scope;
 
 	tmp_result.root_symtab = root_scope;
 	struct mCc_ast_visitor visitor = symtab_visitor(curr_scope_ptr);
