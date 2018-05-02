4e856b586341c0b4a4fe78da727c856af93b63f5
Richard <rkromoser@gmail.com>
2018-05-02 14:38:15 +0200

    removed return fix from 3 commits before

--- src/symtab.c
+++ src/symtab.c
@@ -384,52 +384,6 @@ mCc_symtab_scope_link_ref_assignment(struct mCc_symtab_scope *self,
 	return MCC_SYMTAB_SCOPE_LINK_ERR_OK;
 }
 
-enum MCC_SYMTAB_SCOPE_LINK_ERROR
-mCc_symtab_scope_link_ref_return(struct mCc_symtab_scope *self,
-                                     struct mCc_ast_statement *stmt)
-{
-    // Get the ID to link
-    struct mCc_ast_identifier *id;
-    switch (stmt->type) {
-        case MCC_AST_STATEMENT_TYPE_RET:
-            if(stmt->ret_val->identifier) {
-                id = stmt->ret_val->identifier;
-            }
-            else { // if the return has no identifier e.g return 3+4
-                return MCC_SYMTAB_SCOPE_LINK_ERR_OK;
-            }
-            break;
-        default:
-            return MCC_SYMTAB_SCOPE_LINK_ERROR_INVALID_AST_OBJECT;
-    }
-    struct mCc_symtab_entry *entry = mCc_symtab_scope_lookup_id(self, id);
-    if (!entry)
-        return MCC_SYMTAB_SCOPE_LINK_ERR_UNDECLARED_ID;
-
-    // Basic error checking, though not full type checking
-    switch (entry->entry_type) {
-        case MCC_SYMTAB_ENTRY_TYPE_FUNC://Fallthrough we can return a func
-
-        case MCC_SYMTAB_ENTRY_TYPE_ARR:
-            // Illegal to assign to an array without also giving an index
-            if (!stmt->lhs_assgn)
-                return MCC_SYMTAB_SCOPE_LINK_ERR_ARR_WITHOUT_BRACKS;
-            break;
-
-        case MCC_SYMTAB_ENTRY_TYPE_VAR: //The if(stmt->type) is not working b=1 will be matched here too TODO think about if this check is necessary
-            // if (stmt->lhs_assgn)
-            //   return MCC_SYMTAB_SCOPE_LINK_ERR_VAR;
-            break;
-    }
-    //Link in identifier
-    id->symtab_ref = entry;
-    stmt->ret_val->identifier = id;
-
-    return MCC_SYMTAB_SCOPE_LINK_ERR_OK;
-}
-
-
-
 /******************************* Destructors */
 static void mCc_symtab_delete_scope(struct mCc_symtab_scope *scope)
 {
