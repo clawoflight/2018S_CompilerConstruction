8c5483976ca03bf603b7e60814f45dcd390c2d65
Richard <rkromoser@gmail.com>
2018-05-03 11:40:40 +0200

    tmp commit to ctd work from desktop, finishing up the valgrind erorrs

--- test/tdd_symtab_typecheck.cpp
+++ test/tdd_symtab_typecheck.cpp
@@ -9,14 +9,16 @@ TEST(TYPE_CHECK, LITERAL)
     struct mCc_ast_expression *expr = mCc_ast_new_expression_literal(lit);
 
     ASSERT_EQ(MCC_AST_TYPE_INT, test_type_check(expr));
+    mCc_ast_delete_expression(expr);
 }
 
 TEST(TYPE_CHECK, IDENTIFIER)
 {
     struct mCc_symtab_scope *scope = mCc_symtab_new_scope_in(NULL, "");
+    struct mCc_ast_identifier *decl_id = mCc_ast_new_identifier((char *)"foo");
     struct mCc_ast_identifier *id = mCc_ast_new_identifier((char *)"foo");
     struct mCc_ast_declaration *decl = mCc_ast_new_declaration(MCC_AST_TYPE_STRING,
-                                                               NULL, id);
+                                                               NULL, decl_id);
     mCc_symtab_scope_add_decl(scope, decl);
     struct mCc_symtab_entry *found =
         mCc_symtab_scope_lookup_id(scope, decl->decl_id);
@@ -24,6 +26,8 @@ TEST(TYPE_CHECK, IDENTIFIER)
     expr->identifier->symtab_ref = found;
     ASSERT_EQ(MCC_AST_TYPE_STRING, test_type_check(expr));
 
+    mCc_ast_delete_declaration(decl);
+    mCc_ast_delete_expression(expr);
     mCc_symtab_delete_all_scopes();
 }
 
@@ -191,6 +195,7 @@ TEST(TYPE_CHECK_ARR_SUBSCR, INT_SUBSCR)
     expr->identifier->symtab_ref = found;
 
     ASSERT_EQ(MCC_AST_TYPE_STRING, test_type_check(expr));
+    mCc_ast_delete_declaration(decl);
     mCc_ast_delete_expression(expr);
     mCc_symtab_delete_all_scopes();
 }
