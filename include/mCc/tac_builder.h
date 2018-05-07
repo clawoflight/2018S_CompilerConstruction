02d35a868e1047ce726867759be23fd9e04d6513
Bennett Piater <bennett@piater.name>
2018-05-07 16:40:20 +0200

    more builder code

---
+++ include/mCc/tac_builder.h
@@ -0,0 +1,20 @@
+/**
+ * @file tac_builder.h
+ * @brief Declarations for the three-address-code builder
+ * @author bennett
+ * @date 2018-04-27
+ */
+#ifndef MCC_TAC_BUILDER_H
+#define MCC_TAC_BUILDER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "tac.h"
+#include "ast.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif // MCC_TAC_BUILDER_H
--- src/tac_builder.c
+++ src/tac_builder.c
@@ -4,7 +4,8 @@
  * @author bennett
  * @date 2018-04-27
  */
-//#include "tac_builder.h"
+#include "mCc/tac.h"
+#include "mCc/tac_builder.h"
 
 static struct mCc_tac_quad_entry *
 mCc_tac_from_expression_binary(struct mCc_tac_program *prog,
@@ -15,104 +16,112 @@ mCc_tac_from_expression_binary(struct mCc_tac_program *prog,
 	struct mCc_tac_quad_entry *result2 =
 	    mCc_tac_from_expression(prog, expr->rhs);
 
-	enum mCc_tac_quad_binary_op operator;
+	enum mCc_tac_quad_binary_op op;
 	switch (expr->op) {
 	case MCC_AST_BINARY_OP_ADD:
-		operator= expr->node.type == FLOAT
-		    ? MCC_TAC_EXP_TYPE_BINARY_OP_FLOAT_ADD
-		    : MCC_TAC_EXP_TYPE_BINARY_OP_ADD;
+		op = expr->node.type == FLOAT ? MCC_TAC_OP_BINARY_FLOAT_ADD
+		                              : MCC_TAC_OP_BINARY_ADD;
 		break;
 	case MCC_AST_BINARY_OP_SUB:
-		operator= expr->node.type == FLOAT
-		    ? MCC_TAC_EXP_TYPE_BINARY_OP_FLOAT_SUB
-		    : MCC_TAC_EXP_TYPE_BINARY_OP_SUB;
+		op = expr->node.type == FLOAT ? MCC_TAC_OP_BINARY_FLOAT_SUB
+		                              : MCC_TAC_OP_BINARY_SUB;
 		break;
 	case MCC_AST_BINARY_OP_MULT:
-		operator= expr->node.type == FLOAT
-		    ? MCC_TAC_EXP_TYPE_BINARY_OP_FLOAT_MUL
-		    : MCC_TAC_EXP_TYPE_BINARY_OP_MUL;
+		op = expr->node.type == FLOAT ? MCC_TAC_OP_BINARY_FLOAT_MUL
+		                              : MCC_TAC_OP_BINARY_MUL;
 		break;
 	case MCC_AST_BINARY_OP_DIV:
-		operator= expr->node.type == FLOAT
-		    ? MCC_TAC_EXP_TYPE_BINARY_OP_FLOAT_DIV
-		    : MCC_TAC_EXP_TYPE_BINARY_OP_DIV;
-	case MCC_AST_BINARY_OP_LT: operator= MCC_TAC_EXP_TYPE_BINARY_OP_LT; break;
-	case MCC_AST_BINARY_OP_GT: operator= MCC_TAC_EXP_TYPE_BINARY_OP_GT; break;
-	case MCC_AST_BINARY_OP_LEG: operator= MCC_TAC_EXP_TYPE_BINARY_OP_LEQ; break;
-	case MCC_AST_BINARY_OP_GEQ: operator= MCC_TAC_EXP_TYPE_BINARY_OP_GEQ; break;
-	case MCC_AST_BINARY_OP_AND: operator= MCC_TAC_EXP_TYPE_BINARY_OP_AND; break;
-	case MCC_AST_BINARY_OP_OR: operator= MCC_TAC_EXP_TYPE_BINARY_OP_OR; break;
-	case MCC_AST_BINARY_OP_EQ: operator= MCC_TAC_EXP_TYPE_BINARY_OP_EQ; break;
-	case MCC_AST_BINARY_OP_NEQ: operator= MCC_TAC_EXP_TYPE_BINARY_OP_NEQ; break;
+		op = expr->node.type == FLOAT ? MCC_TAC_OP_BINARY_FLOAT_DIV
+		                              : MCC_TAC_OP_BINARY_DIV;
+	case MCC_AST_BINARY_OP_LT: op = MCC_TAC_OP_BINARY_LT; break;
+	case MCC_AST_BINARY_OP_GT: op = MCC_TAC_OP_BINARY_GT; break;
+	case MCC_AST_BINARY_OP_LEQ: op = MCC_TAC_OP_BINARY_LEQ; break;
+	case MCC_AST_BINARY_OP_GEQ: op = MCC_TAC_OP_BINARY_GEQ; break;
+	case MCC_AST_BINARY_OP_AND: op = MCC_TAC_OP_BINARY_AND; break;
+	case MCC_AST_BINARY_OP_OR: op = MCC_TAC_OP_BINARY_OR; break;
+	case MCC_AST_BINARY_OP_EQ: op = MCC_TAC_OP_BINARY_EQ; break;
+	case MCC_AST_BINARY_OP_NEQ: op = MCC_TAC_OP_BINARY_NEQ; break;
 	}
 
 	struct mCc_tac_entry *new_result = TODO_get_new_temporary();
 
-	struct mCc_tac_quad binary_op =
-	    mCc_tac_quad_new_op_binary(operator, result1, result2, new_result);
+	struct mCc_tac_quad *binary_op =
+	    mCc_tac_quad_new_op_binary(op, result1, result2, new_result);
 	// TODO error checking
 	mCc_tac_program_add_quad(prog, binary_op);
 	// TODO error checking
 }
 
-static struct mCc_tac_program *
-mCc_tac_from_expression_unary(struct mCc_ast_expression *expr)
+static struct mCc_tac_quad_entry *
+mCc_tac_from_expression_unary(struct mCc_tac_program *prog,
+                              struct mCc_ast_expression *expr)
 {
-	// rec. create mCc_tac_program for operand
-	// create quad [unary_op, result_of_prog]
+	enum mCc_tac_quad_unary_op op;
+	switch (expr->unary_op) {
+	case MCC_AST_UNARY_OP_NEG: op = MCC_TAC_OP_UNARY_NEG; break;
+	case MCC_AST_UNARY_OP_NOT: op = MCC_TAC_OP_UNARY_NOT; break;
+	}
 
-	// create new program with memory for program + 1 quad
-	// copy all into new program
-	// (destroy rec. program without destroying it's quads)
+	struct mCc_tac_entry *result =
+	    mCc_tac_from_expression(prog, expr->unary_expression);
+
+	result = mCc_tac_quad_new_op_unary(op, result, result);
+	mCc_tac_program_add_quad(prog, result);
+	// TODO error checking
 }
 
-static struct mCc_tac_program *
+static struct mCc_tac_quad_entry *
 mCc_tac_from_expression_arr_subscr(struct mCc_ast_expression *expr)
 {
 	// rec. create mCc_tac_program for array index
 	// create quad [load, result_of_prog]
 }
 
-static int
-mCc_tac_from_expression_call(struct mCc_tac_program *prog, struct mCc_ast_expression *expr)
+static struct mCc_tac_quad_entry *
+mCc_tac_from_expression_call(struct mCc_tac_program *prog,
+                             struct mCc_ast_expression *expr)
 {
 	// Compute all params in reverse order and push them
-	// TODO: I fear that we will need to first compute all params, then push them.
-	// That would make this far more annoying - use variable-length array to store results maybe?
-	for (unsigned int i = expr->arguments->expression_count-1; i >= 0; --i) {
-		struct mCc_tac_quad_result *param_temporary = mCc_tac_from_expression(prog, expr->arguments->expressions[i]);
+	// TODO: I fear that we will need to first compute all params, then push
+	// them. That would make this far more annoying - use variable-length array
+	// to store results maybe?
+	for (unsigned int i = expr->arguments->expression_count - 1; i >= 0; --i) {
+		struct mCc_tac_quad_result *param_temporary =
+		    mCc_tac_from_expression(prog, expr->arguments->expressions[i]);
 		struct mCc_tac_quad *param = mCc_tac_quad_new_param(param_temporary);
-		mCc_tac_program_add_quad(param);
+		mCc_tac_program_add_quad(prog, param);
 	}
 
 	struct mCc_tac_label *label_fun = TODO_get_label_for_function(expr->f_name);
 	struct mCc_tac_quad *jump_to_fun = mCc_tac_quad_new_jump(label_fun);
-	mCc_tac_program_add_quad(jump_to_fun);
+	mCc_tac_program_add_quad(prog, jump_to_fun);
 }
 
 // TODO think about if without else - which label to remove
-static int
-mCc_tac_from_statement_if(struct mCc_tac_program *prog, struct mCc_ast_statement *stmt)
+static struct mCc_tac_quad_entry *
+mCc_tac_from_statement_if(struct mCc_tac_program *prog,
+                          struct mCc_ast_statement *stmt)
 {
 	// TODO error handling everywhere
 	struct mCc_tac_label *label_else = TODO_get_new_label();
 	struct mCc_tac_label *label_after_if = TODO_get_new_label();
 
-	struct mCc_tac_quad_result *cond = mCc_tac_from_expression(prog, stmt->if_cond);
-	struct mCc_tac_quad *jump_to_else = mCc_tac_quad_new_jumpfalse(cond, label_else);
+	struct mCc_tac_quad_entry *cond =
+	    mCc_tac_from_expression(prog, stmt->if_cond);
+	struct mCc_tac_quad *jump_to_else =
+	    mCc_tac_quad_new_jumpfalse(cond, label_else);
 	mCc_tac_program_add_quad(prog, jump_to_else);
 	mCc_tac_from_stmt(prog, stmt->if_stmt);
 
 	struct mCc_tac_quad *jump_after_if = mCc_tac_quad_new_jump(label_after_if);
-	mCc_tac_program_add_quad(jump_after_if);
+	mCc_tac_program_add_quad(prog, jump_after_if);
 
 	mCc_tac_from_stmt(prog, stmt->else_stmt);
-	mCc_tac_program_add_quad(label_after_if);
+	mCc_tac_program_add_quad(prog, label_after_if);
 }
 
-static int
-mCc_tac_from_statement_while(struct mCc_tac_program *prog,
-                             struct mCc_ast_statement *stmt)
+static int mCc_tac_from_statement_while(struct mCc_tac_program *prog,
+                                        struct mCc_ast_statement *stmt)
 {
 	// TODO error handling everywhere
 	struct mCc_tac_label *label_cond = TODO_get_new_label();
@@ -131,8 +140,8 @@ mCc_tac_from_statement_while(struct mCc_tac_program *prog,
 	mCc_tac_program_add_quad(prog, label_after_while);
 }
 
-static struct mCc_tac_program *
-mCc_tac_from_statement_return(struct mCc_ast_statement *stmt)
+static int mCc_tac_from_statement_return(struct mCc_tac_program *prog,
+                                         struct mCc_ast_statement *stmt)
 {
 	// create mCc_tac_program for return expression
 	// TODO will probably need a quad for return?
